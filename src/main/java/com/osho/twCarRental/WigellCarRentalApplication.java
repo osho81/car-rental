dd7ade74989a6c406fbd5127c8c7b1fd9cddac6a
osho81 <ossi.hour@gmail.com>
2025-08-03 21:45:57 +0200

    Disable keycloak, security and eureka, final.

--- src/main/java/com/osho/twCarRental/WigellCarRentalApplication.java
+++ src/main/java/com/osho/twCarRental/WigellCarRentalApplication.java
@@ -3,12 +3,12 @@ package com.osho.twCarRental;
 //import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class WigellCarRentalApplication {
 
@@ -17,7 +17,7 @@ public class WigellCarRentalApplication {
     }
 
     @Bean
-    @LoadBalanced
+//    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
